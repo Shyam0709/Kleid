6f54d1d580ea6c2edf21be99a851ddb4c3e00652
SHYAM SUNDAR <166678415+Shyam0709@users.noreply.github.com>
2025-07-27 13:39:42 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,9 +12,9 @@ import "react-toastify/dist/ReactToastify.css";
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const currency = (price) => {
-  return new Intl.NumberFormat("en-US", {
+  return new Intl.NumberFormat("en-IN", {
     style: "currency",
-    currency: "Rs",
+    currency: "INR",
   }).format(price);
 };
 
